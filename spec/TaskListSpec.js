b4e70e73476a4e5e153cf7a6510abdc06d315220
Stephanie Bernhard <mail@stephaniebernhard.ch>
2017-03-12 13:50:56 +0100

    added test and implentation for removing a task

--- spec/TaskListSpec.js
+++ spec/TaskListSpec.js
@@ -5,10 +5,17 @@ describe("addTask", function() {
 	beforeEach(function() {
 		tasklist = new TaskList("Samplelist");
 		testtask = new Task("Testtask");
+		tasklist.addTask(testtask);
 	});
 
-	it("tasklist should contain testtask after adding testtask to tasklist", function() {
-		tasklist.addTask(testtask);
-		expect(tasklist.taskarray).toContain(testtask);
+	it("should contain testaddtask after adding testaddtask", function() {
+		testaddtask = new Task("Testaddtask");
+		tasklist.addTask(testaddtask);
+		expect(tasklist.taskarray).toContain(testaddtask);
+	});
+
+	it("should not contain testtask after removing testtask", function() {
+		tasklist.removeTask(testtask);
+		expect(tasklist.taskarray).not.toContain(testtask);
 	});
 });
--- src/TaskList.js
+++ src/TaskList.js
@@ -5,5 +5,9 @@ var TaskList = function(title) {
 	this.addTask = function(task) {
 		this.taskarray.push(task);
 	}
+
+	this.removeTask = function(task) {
+		this.taskarray = this.taskarray.filter(function(t) {t!=task});
+	}
 }
 
